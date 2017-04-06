91057dfcc3871df456c66ee7a41923d65fa9a408
KCostanzo <kstanz22@gmail.com>
2017-04-06 10:35:57 -0700

    clock running

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -5,7 +5,7 @@ export default class Clock extends React.Component {
     super(props);
     const date = new Date();
 
-    var hour = date.getHours();
+    let hour = date.getHours();
     if (hour === 0) {
       hour = 12;
     } else if (hour > 12) {
@@ -24,7 +24,51 @@ export default class Clock extends React.Component {
   }
 
   incrementor() {
+    setInterval(this.startSeconds.bind(this), 1000);
+  }
 
+  startSeconds() {
+    let {sec} = this.state;
+    if (sec < 59) {
+      this.setState({ sec: sec += 1 });
+    } else if (sec === 59) {
+      this.setState({ sec: 0 });
+      this.addMin();
+    }
+  }
+
+  addMin() {
+    let {min} = this.state;
+    if (min < 59) {
+      this.setState({ min: min += 1 });
+    } else if (min === 59) {
+      this.setState({ min: 0 });
+      this.addHour();
+    }
+  }
+
+  addHour() {
+    let { hour } = this.state;
+    if (hour < 12) {
+      if (hour === 11) {
+        this.toggleAmPm();
+      }
+      this.setState({ hour: hour+= 1});
+    } else if (hour === 12){
+      this.setState({ hour: 1});
+    }
+  }
+
+  toggleAmPm() {
+    this.setState({ morning: !this.state.morning });
+  }
+
+  amOrPm() {
+    if (this.state.morning) {
+      return "AM";
+    } else {
+      return "PM";
+    }
   }
 
   render() {
@@ -37,6 +81,9 @@ export default class Clock extends React.Component {
           {this.state.sec}
           <br/>
           {this.state.morning}
+          <div className="AMPM">
+          {this.amOrPm()}
+          </div>
       </div>
     )
   }
